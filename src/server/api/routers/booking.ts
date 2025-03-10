abc1c1604b5b26b06450dc5810a576ec657a5c39
keithxun <keithonghx@gmail.com>
2025-03-10 14:43:29 +0800

    Booking api established

---
+++ src/server/api/routers/booking.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+export const bookingRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  create: publicProcedure
+    .input(
+      z.object({
+        // Expect a valid date (passed as a JS Date)
+        slot: z.date(),
+        userId: z.number(),
+        facilityId: z.number(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const booking = await ctx.db.booking.create({
+        data: input,
+      });
+      return booking;
+    }),
+
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    return await ctx.db.booking.findMany();
+  }),
+});
